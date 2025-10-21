baffc3e32881c722c4de3ce8adf8a0da88ece6c4
S2Ruby <jwzang1004@gmail.com>
2025-10-21 16:08:51 +0900

    fix2

--- taylor_multiprocess.c
+++ taylor_multiprocess.c
@@ -1,62 +1,73 @@
 #include <stdio.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #define N 4
+#define MAXLINE 100
 
 void sinx_taylor(int num_elements, int terms, double* x, double* result)
 {
-    for(int i=0; i<num_elements; i++) {
-        double value = x[i];
-        double numer = x[i] * x[i] * x[i];
-        double denom = 6.;
-        int sign = -1;
-        for(int j=1; j<=terms; j++) {
-            value += (double)sign * numer / denom;
-            numer *= x[i] * x[i];
-            denom *= (2.*(double)j+2.) * (2.*(double)j+3.);
-            sign *= -1;
+        int fd[2*N];
+        int pid;
+        char message[MAXLINE], line[MAXLINE];
+        int length;
+        int child_id;
+
+        for(int i = 0; i<num_elements; i++){
+                pipe(fd + 2*i);
+
+                child_id = i;
+                pid = fork();
+                if(pid ==0) {                        break;
+                } else {
+                        close(fd[2*i+1]);
+                }
         }
-        result[i] = value;
-    }
+
+        if(pid ==0){
+                close(fd[2*child_id]);
+                double value = x[child_id];
+                double numer = x[child_id] * x[child_id] * x[child_id];
+                double denom = 6.;
+                int sign = -1;
+
+                for(int j=1; j<=terms; j++){
+                        value += (double)sign * numer / denom;
+                        numer *= x[child_id] * x[child_id];
+                        denom *= (2.*(double)j+2.) * (2.*(double)j +3);
+                        sign *= -1;
+                }
+                result[child_id] = value;
+                sprintf(message, "%lf", result[child_id]);
+                length = strlen(message)+1;
+                write(fd[2*child_id+1], message, length);
+                exit(child_id);
+        }else {
+                for(int i=0;i<num_elements; i++){
+                        int status;
+                        wait(&status);
+                        int child_id= status >> 8;
+                        read(fd[2*child_id], line, MAXLINE);
+                        result[child_id] = atoi(line);
+                }
+
+        }
+
 }
 
 int main()
 {
-    double x[N] = {0, M_PI/6., M_PI/3., 0.134};
-    double res[N];
-    int fd[N][2];
+        double x[N] = {0, M_PI/6., M_PI/3., 0.134};
+        double res[N];
 
-    for(int i=0; i<N; i++) {
-        pipe(fd[i]);
-        pid_t pid = fork();
-
-        if(pid == 0) { 
-            close(fd[i][0]);
-
-            double result;
-            sinx_taylor(1, 3, &x[i], &result);
-            write(fd[i][1], &result, sizeof(result));
-
-            close(fd[i][1]);
-            return 0;
+        sinx_taylor(N, 3, x, res);
+        for(int i = 0; i<N; i++){
+                printf("sin(%.2f) by Taylor Series = %f\n",x[i], res[i]);
+                printf("sin(%.2f) =  %f\n", x[i], sin(x[i]));
         }
-        else if(pid > 0) {
-            close(fd[i][1]);
-        }
-    }
-
-    for(int i=0; i<N; i++) {
-        read(fd[i][0], &res[i], sizeof(res[i]));
-        close(fd[i][0]);
-        wait(NULL);
-    }
-
-    for(int i=0; i<N; i++) {
-        printf("sin(%.2f) by Taylor series = %f\n", x[i], res[i]);
-        printf("sin(%.2f) = %f\n", x[i], sin(x[i]));
-    }
-
-    return 0;
+        return 0;
 }
